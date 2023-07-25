47b5ff20726f234780b300ac8e97c6d17055d486
Paul Hwang <paulsoohyunhwang@gmail.com>
2023-07-24 22:14:58 -0400

    banner part 1

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import './HomeScreen.css'
-import Nav from './Nav'
+import React from 'react';
+import './HomeScreen.css';
+import Nav from './Nav';
+import Banner from './Banner';
+
 function HomeScreen() {
   return (
     <div className="homeScreen">
       <Nav/>
-    {/* Banner */}
+      <Banner/>
     {/* Row */}
     </div>
   )
 }
 
-export default HomeScreen
+export default HomeScreen;
