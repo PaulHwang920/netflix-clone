25b65297c29cfc7a376900344943ea87c7560f02
Paul Hwang <paulsoohyunhwang@gmail.com>
2023-07-24 21:36:46 -0400

    Base nav bar

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDxQlUck5BUAnqSo89vVh8RcZX0LQGLUkc",
+  authDomain: "netflix-clone-d3884.firebaseapp.com",
+  projectId: "netflix-clone-d3884",
+  storageBucket: "netflix-clone-d3884.appspot.com",
+  messagingSenderId: "470083613367",
+  appId: "1:470083613367:web:4f1a3037343b805f7d9332"
+};
